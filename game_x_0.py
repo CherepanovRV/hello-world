9a633e847beda0260509f975eaef4037fef13c89
Roman <cherepanov_17_81@list.ru>
2025-01-28 12:12:56 +0700

    дополнено пояснение, проведено редактирование

--- game_x_0.py
+++ game_x_0.py
@@ -1,44 +1,45 @@
-
 # Данная игра практически вся взята из интернета
 # я пытался реализовать игру с неограниченным полем, второй файл в данном репозитории, как раз ее и представляет
 # https://github.com/CherepanovRV/hello-world/blob/main/x%20and%200.py
 # единственное, что не могу реализовать это выход программы при определнии победы.
 
 
+board = list(range(1, 10))
 
 
-board = list(range(1,10))
+def draw_board(board):  # трисовка поля
+    print("-" * 13)
+    for i in range(3):
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
 
-def draw_board(board): # трисовка поля
-   print("-" * 13)
-   for i in range(3):
-      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-      print("-" * 13)
 
-def take_input(player_token): # ввод 'x' или 'o' в поле с соответсвующими проверками
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
-         continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
-            valid = True
-         else:
-            print("Эта клетка уже занята!")
-      else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+def take_input(player_token):  # ввод 'x' или 'o' в поле с соответсвующими проверками
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer - 1]) not in "XO"):
+                board[player_answer - 1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
 
-def check_win(board): # проверка выишрышных комбинаций
-   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
-   return False
+
+def check_win(board):  # проверка выишрышных комбинаций
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
 
 def main(board):
     counter = 0
@@ -46,20 +47,22 @@ def main(board):
     while not win:
         draw_board(board)
         if counter % 2 == 0:
-           take_input("X")
+            take_input("X")
         else:
-           take_input("O")
+            take_input("O")
         counter += 1
         if counter > 4:
-           tmp = check_win(board)
-           if tmp:
-              print(tmp, "выиграл!")
-              win = True
-              break
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, "выиграл!")
+                win = True
+                break
         if counter == 9:
             print("Ничья!")
             break
     draw_board(board)
+
+
 main(board)
 
 input("Нажмите Enter для выхода!")
